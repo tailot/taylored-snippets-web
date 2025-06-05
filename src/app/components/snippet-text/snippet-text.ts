aa7ea69cd6f1da23b20fcddc1c5ef33fd7721ec0
vincenzo <tailot@gmail.com>
2025-06-05 16:36:01 +0200

    Fix imports

--- src/app/components/snippet-text/snippet-text.ts
+++ src/app/components/snippet-text/snippet-text.ts
@@ -1,6 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { Snippet } from '../sheet/sheet';
 

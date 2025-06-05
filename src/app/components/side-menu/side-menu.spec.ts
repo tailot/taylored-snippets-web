df080480487a792176c38e8be7db199ef0915ebd
vincenzo <tailot@gmail.com>
2025-06-05 19:10:08 +0200

    Fix CI

--- src/app/components/side-menu/side-menu.spec.ts
+++ src/app/components/side-menu/side-menu.spec.ts
@@ -1,18 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
 
-import { SideMenu } from './side-menu';
+import { SideMenuComponent } from './side-menu';
 
-describe('SideMenu', () => {
-  let component: SideMenu;
-  let fixture: ComponentFixture<SideMenu>;
+describe('SideMenuComponent', () => {
+  let component: SideMenuComponent;
+  let fixture: ComponentFixture<SideMenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SideMenu]
+      imports: [SideMenuComponent],
+      providers: [provideZonelessChangeDetection()]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SideMenu);
+    fixture = TestBed.createComponent(SideMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
